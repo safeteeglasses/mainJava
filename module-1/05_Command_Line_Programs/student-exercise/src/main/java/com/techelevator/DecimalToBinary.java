ce8b5dccf08306f8ca1f2357b47f6a6249c00bdd
Thomas Richards <richardst3778@gmail.com>
2019-01-21 10:14:34 -0500

    command line programs

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -6,30 +6,40 @@ public class DecimalToBinary {
 
 	public static void main(String[] args) {
 		
-		Scanner keyBoard = new Scanner(System.in);
-		
-		String aLine = "";
-		double binaryNumber = 0;
-		
-		System.out.println("Please enter in a decimal number.");
-		aLine = keyBoard.nextLine();
-		binaryNumber = Double.parseDouble(aLine);
-		
-		double n = 0;
-		double[] binaryNum = new double[100]; //<--- this is the array created that will hold binary number
-		int i = 0; //counter number going through the for loop
-		while (n > 0) {
-			binaryNum [i] = n % 2;
-			n = n / 2;
-			i ++;
-		}
-		
-		for (double j = i - 1; j >= 0; j--) {
-			System.out.println(binaryNum [i]);
-			aLine = keyBoard.nextLine();
-			binaryNumber = Double.parseDouble(aLine);
-		}
-       
-	}
+		 Scanner keyBoard = new Scanner(System.in);
+		 
+		 int number = 0;
+		 String aLine = "";
+		 
+		 
+		 
+		 
+	        System.out.println("Please enter in a series of decimal values (separated by spaces):");
+	        number = keyBoard.nextInt();
+	        
+	        
+	        
+	        if (number < 0) {
+	            System.out.println("Error: Not a positive integer");
+	        } else { 
 
-}
+	            System.out.print( number + " in binary is " );
+	            printBinaryform(number);
+	        }
+	        
+	    }
+	
+			private static void printBinaryform(int number) {
+	        int remainder = 0;
+
+	        if (number <= 1) {
+	            System.out.print(number);
+	            return;   
+	        }
+
+	        remainder = number % 2; 
+	        printBinaryform(number >> 1);
+	        System.out.print(remainder);
+	    }
+		
+	}
