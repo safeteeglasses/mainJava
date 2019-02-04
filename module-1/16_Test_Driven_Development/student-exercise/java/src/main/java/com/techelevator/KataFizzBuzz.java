ed9027a57d7ad9b8f8cd362dc1827eb831f67136
Thomas Richards <richardst3778@gmail.com>
2019-02-04 13:51:55 -0500

    test one

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,13 @@
 package com.techelevator;
 
 public class KataFizzBuzz {
+	
+	public static String fizzBuzz(int num) {
+		if(num == 1) {
+			return "1";
+		}
+		
+		return "";
+	}
 
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -5,4 +5,11 @@ import static org.junit.Assert.*;
 
 public class KataFizzBuzzTest {
 
+	
+	@Test 
+	public void returns_int_1() {
+		assertEquals("String does not return 1", ("1"), KataFizzBuzz.fizzBuzz(1));
+	}
+	
+	
 }
