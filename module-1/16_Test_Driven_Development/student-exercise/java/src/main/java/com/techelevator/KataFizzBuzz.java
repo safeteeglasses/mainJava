734e8fa41d1f29e3880ee17dccb8e602022308c9
Thomas Richards <richardst3778@gmail.com>
2019-02-04 14:17:39 -0500

    test 5

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -4,6 +4,9 @@ public class KataFizzBuzz {
 	
 	public static String fizzBuzz(int num) {
 		
+		if (num == 0) {
+			return "" + num +"";
+		}
 		if(num == 1) {
 			return "1";
 		}
@@ -15,6 +18,9 @@ public class KataFizzBuzz {
 		}
 		if (num % 5 == 0) {
 			return "Buzz";
+		}
+		if (num > 100) {
+			return "";
 		}
 		
 		return "" + num + "";
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -12,6 +12,7 @@ public class KataFizzBuzzTest {
 		assertEquals("String does not return 2", ("2"), KataFizzBuzz.fizzBuzz(2));
 		assertEquals("String does not return 4", ("4"), KataFizzBuzz.fizzBuzz(4));
 	}
+	@Test
 	public void multiple_of_3_and_5_returns_fizzbuzz() {
 		assertEquals("String does not return FizzBuzz", ("FizzBuzz"), KataFizzBuzz.fizzBuzz(15));
 		assertEquals("String does not return FizzBuzz", ("FizzBuzz"), KataFizzBuzz.fizzBuzz(30));
@@ -25,5 +26,9 @@ public class KataFizzBuzzTest {
 	public void multiple_of_only_5() {
 		assertEquals("String does not return Buzz", ("Buzz"), KataFizzBuzz.fizzBuzz(50));
 		assertEquals("String does not return Buzz", ("Buzz"), KataFizzBuzz.fizzBuzz(100));
+	}
+	@Test
+	public void passed_in_0() {
+		assertEquals("String does not return empty String", ("0"), KataFizzBuzz.fizzBuzz(0));
 	}
 }
