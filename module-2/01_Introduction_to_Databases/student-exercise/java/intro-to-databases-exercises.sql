a1d6030b57b744f39bbdec756e69d3391abf5d02
Thomas Richards <richardst3778@gmail.com>
2019-02-11 15:27:43 -0500

    finished 22 exercises

--- module-2/01_Introduction_to_Databases/student-exercise/java/intro-to-databases-exercises.sql
+++ module-2/01_Introduction_to_Databases/student-exercise/java/intro-to-databases-exercises.sql
@@ -5,45 +5,112 @@
 --   The value immediately after the problem statement is the expected number of rows that should be returned by the query.
 
 -- 1. The name and population of all cities in Ontario, Canada (27 rows)
-
+Select name, population
+From city
+Where district = 'Ontario'
+;
 -- 2. The name and population of all cities in Montana (1 row)
-
+Select name, population
+From city
+Where district = 'Montana'
+;
 -- 3. The name, form of government, and head of state of all countries in Europe (46 rows)
-
+Select name, governmentform, headofstate, continent 
+From country
+Where continent = 'Europe'
+;
 -- 4. The name, population, surface area, and average life expectancy of all countries in Asia (51 rows)
-
+Select name, population, surfacearea, lifeexpectancy
+From country
+Where continent = 'Asia'
+;
 -- 5. The name, country code, and population of all cities with a population greater than 8 million people (10 rows)
-
+Select name, countrycode, population
+From city
+Where population > 8000000
+;
 -- 6. The name, country code, and population of all cities with a population less than one thousand people (11 rows)
-
+Select name, countrycode, population
+From city
+Where population < 1000
+;
 -- 7. The name, continent, and GNP of all countries with a GNP greater than one trillion dollars (6 rows)
-
+Select name, continent, gnp
+From country
+Where gnp > 1000000
+;
 -- 8. The name, continent, population, GNP, and average life expectancy of all countries with an average life expectancy greater than 80 years (5 rows)
-
+Select name, continent, population, gnp, lifeexpectancy
+From country
+Where lifeexpectancy > 80
+;
 -- 9. The name and population of all cities in the USA with a population of greater than 1 million people (9 rows)
-
+Select name, population 
+From city
+Where countrycode = 'USA' and population > 1000000
+;
 -- 10. The name and population of all cities in China with a population of greater than 1 million people (35 rows)
-
+Select name, population
+From city
+Where countrycode = 'CHN' and population > 1000000
+;
 -- 11. The name and region of all countries in North or South America (51 rows)
-
+Select name, region
+From country
+Where continent = 'North America' or continent = 'South America'
+;
 -- 12. The name, continent, and head of state of all countries whose form of government is a monarchy (43 rows)
-
+Select name, continent, headofstate
+From country
+Where governmentform like '%Monarchy%'
+;
 -- 13. The name of all cities in the USA with a population between 1 million and 2 million people (6 rows) 
-
+Select name
+From city
+Where countrycode = 'USA' and population between 1000000 and 2000000
+;
 -- 14. The name and region of all countries in North or South America except for countries in the Caribbean (27 rows)
-
+Select name, region
+From country
+Where (continent = 'North America' or continent = 'South America') and region != 'Caribbean'
+;
 -- 15. The name, population, and GNP of all countries with a GNP greater than $1 trillion dollars and a population of less than 100 million people (4 rows)
-
+Select name, population, gnp
+From country
+Where gnp > 1000000 and population < 100000000
+;
 -- 16. The name and population of all cities in Texas that have a population of greater than 1 million people (3 rows)
-
+Select name, population
+From city
+Where district = 'Texas' and population > 1000000
+;
 -- 17. The name and average life expectancy of all countries on the continent of Oceania (28 rows)
-
+Select name, lifeexpectancy
+From country
+Where continent = 'Oceania' 
+;
 -- 18. The name and average life expectancy of all countries on the continent of Oceania for which an average life expectancy has been provided (i.e. not equal to null) (20 rows)
-
+Select name, lifeexpectancy
+From country
+Where continent = 'Oceania' and lifeexpectancy is not null
+;
 -- 19. The name of all countries on the continent of Oceania for which an average life expectancy has not been provided (i.e. equal to null) (8 rows)
-
+Select name, lifeexpectancy
+From country
+Where continent = 'Oceania' and lifeexpectancy is null
+;
 -- 20. The name, continent, GNP, and average life expectancy of all countries that have an average life expectancy of at least 70 years and a GNP between $1 million and $100 million dollars (3 rows)
-
+Select name, continent, gnp, lifeexpectancy
+From country
+Where (lifeexpectancy >= 70) and (gnp between 1 and 100)
+; 
 -- 21. The per capita GNP (i.e. GNP divided by population) in US Dollars of all countries in Europe (46 rows)
-
+Select gnp/population as capita_gnp
+From country
+Where continent = 'Europe'
+;
 -- 22. The number of years since independence for all countries that have a year of independence (192 rows)
+Select 2019-indepyear as years_since_independence
+From country
+Where indepyear is not null 
+;
--- module-2/01_Introduction_to_Databases/student-lecture/lecture-queries.sql
+++ module-2/01_Introduction_to_Databases/student-lecture/lecture-queries.sql
@@ -38,44 +38,77 @@
 ------------------------------------------------------------------------------------------------------
 --
 -- Show all the rows and all the columns for all countries
-
-
+SELECT *        --all the columns
+FROM   country  --from this table
+;
 -- Show the names for all countries
-
-
+SELECT name
+FROM country
+;
 -- Show the name and population of all countries
-
-
+SELECT name, population
+FROM country
+;
 -- Show all columns from the city table
-
-
+SELECT *
+FROM city
+;
 -- Show the cities in Ohio
- 
-
+SELECT name
+FROM city
+WHERE district = 'Ohio' -- in SQL string literal is enclosed in ' '(NOT double quotes)
+;
 -- Show countries that gained independence in the year 1776
-
-
+SELECT name
+FROM country
+WHERE indepyear = 1776 -- numeric values are not in aposts
+;
 -- Show countries not in Asia
-
-
+SELECT name, continent
+FROM country
+WHERE continent != 'Asia'
+;
 -- Show countries that do not have an independence year
-
-
+Select name, indepyear
+from country
+where indepyear is null
+;
 -- Show countries that do have an indepenShowr
-
-
+Select name, indepyear
+from country
+where indepyear is not null
+;
 -- Show countries that have a population greater than 5 million
-
-
+Select name, population
+From country
+Where population > 5000000
+;
 -- Show cities in Ohio and Population greater than 400,000
-
-
+Select name, population
+From city
+Where population > 400000
+ and district = 'Ohio'
+;
 -- Show country names on the continent North America or South America
-
-
+Select name, continent
+From country
+Where continent in ('North America', 'South America')
+;
 -- Show the population, life expectancy, and population per area (population / surfacearea)
-
-
-
-
-
+Select name, population, lifeexpectancy, 
+       population / surfacearea -- calculations go on select line
+From country                    -- as provides a new name for a column in derived column
+;
+Select name, population
+from country
+where population between 1000000 and 2000000 -- can do a range check
+;
+Select name, district
+from city
+where district between 'Ohio' and 'Utah'
+and countrycode = 'USA'
+;
+Select name
+from country
+where name like 'United%'
+;
