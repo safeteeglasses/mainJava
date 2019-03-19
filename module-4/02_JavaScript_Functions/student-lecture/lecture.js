d41e04b8dd516ea0b26156ff6849c92d1517184e
Frank Fella <frank@techelevator.com>
2019-03-19 05:47:07 -0400

    Instructor Push 03/19/2019 05:47:06

---
+++ module-4/02_JavaScript_Functions/student-lecture/lecture.js
@@ -0,0 +1,89 @@
+/*******************************************************************************************
+ * JavaScript - Functions - Student-Lecture
+ ******************************************************************************************/
+/**
+ * All named functions will have the function keyword and
+ * a name followed by parentheses.
+ */
+function returnOne() {
+  return 1;
+}
+
+/**
+ * Functions can also take parameters. These are just variables that are filled
+ * in by whoever is calling the function.
+ *
+ * Also, we don't *have* to return anything from the actual function.
+ *
+ * @param {any} value the value to print to the console
+ */
+function printToConsole(value) {
+  console.log(value);
+}
+
+/**
+ * Multiply two numbers together. But what happens of we don't pass
+ * a value in? What happens if the value is not a number?
+ *
+ * @param {number} firstParameter the first parameter to multiply
+ * @param {number} secondParameter the second parameter to multiply
+ */
+
+/**
+ * This version makes sure that no parameters are ever missing. If
+ * someone calls this function without parameters, we default the
+ * values to 0. However, it is impossible in JavaScript to prevent
+ * someone from calling this function with data that is not a number.
+ *
+ * @param {number} [firstParameter=0] the first parameter to multiply
+ * @param {number} [secondParameter=0] the second parameter to multiply
+ */
+
+/**
+ * Scope is defined as where a variable is available to be used.
+ *
+ * If a variable is declare inside of a block, it will only exist in
+ * that block and any block underneath it. Once the block that the
+ * variable was defined in ends, the variable disappears.
+ */
+function scopeTest() {
+  // This variable will always be in scope in this function
+  let inScopeInScopeTest = true;
+
+  {
+    // this variable lives inside this block and doesn't
+    // exist outside of the block
+    let scopedToBlock = inScopeInScopeTest;
+  }
+
+  // scopedToBlock doesn't exist here so an error will be thrown
+  if (inScopeInScopeTest && scopedToBlock) {
+    console.log("This won't print!");
+  }
+}
+
+function createSentenceFromUser(name, age, listOfQuirks = [], separator = ', ') {
+  let description = `${name} is currently ${age} years old. Their quirks are: `;
+  return description + listOfQuirks.join(separator);
+}
+
+/**
+ * Takes an array and, using the power of anonymous functions, generates
+ * their sum.
+ *
+ * @param {number[]} numbersToSum numbers to add up
+ * @returns {number} sum of all the numbers
+ */
+function sumAllNumbers(numbersToSum) {
+  return numbersToSum.reduce();
+}
+
+/**
+ * Takes an array and returns a new array of only numbers that are
+ * multiples of 3
+ *
+ * @param {number[]} numbersToFilter numbers to filter through
+ * @returns {number[]} a new array with only those numbers that are
+ *   multiples of 3
+ */
+function allDivisibleByThree(numbersToFilter) {}
