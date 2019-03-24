7b0630657fa17fe0c224c40f6f129dd419f6bb80
Frank Fella <frank@techelevator.com>
2019-03-24 18:37:35 -0400

    Instructor Push 03/24/2019 18:37:35

---
+++ module-4/07_Vue_Data_Binding/student-exercise/shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="shopping-list">
+        <h1>My Shopping List</h1>
+        <ul>
+            <li class="completed">Oatmeal</li>
+            <li class="completed">Milk</li>
+            <li class="completed">Banana</li>
+            <li>Strawberries</li>
+            <li>Lunch Meat</li>
+            <li>Bread</li>
+            <li>Grapes</li>
+            <li>Steak</li>
+            <li>Salad</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.shopping-list {
+    width:450px;
+    background: #fff;
+    margin: 50px auto;
+    font-family: 'Roboto Condensed', sans-serif;
+    border-radius: 10px;
+}
+h1 {
+    background:#f2f2f2;
+    color:#5c8578;
+    padding:10px;
+    font-size:24px;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+ul {
+    list-style-type: none;
+    margin:0px;
+    padding:0px;
+}
+li {
+    font-size: 24px;
+    border-bottom:1px solid #f2f2f2;
+    padding:10px 20px;
+}
+li:last-child{
+    border:0px;
+}
+li.completed {
+    text-decoration: line-through;
+    color:darkgrey;
+}
+</style>
