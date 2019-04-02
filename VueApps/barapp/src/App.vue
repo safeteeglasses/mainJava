12c35828d07760f57a2fb7bb5f40145c5785c8b0
Thomas Richards <richardst3778@gmail.com>
2019-04-02 09:35:40 -0400

    friday

---
+++ VueApps/barapp/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <drink-list/>
+    <search-box
+      search-url="https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+      v-on:search-results="handleResults"> <!-- when search-results event happens call handleResults method -->
+    </search-box>
+    <results-display v-bind:results="searchResults"></results-display>
+  </div>
+    
+  
+</template>
+
+<script>
+import DrinkList from '@/components/DrinkList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    DrinkList
+  },
+  data() {
+    return {
+      searchResults: { "Results": [] } // searchResults is an array
+    }
+  },
+  methods: {
+    handleResults(newResults) {
+      this.searchResults = newResults;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueApps/barapp/src/components/DrinkList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="Tom's Bar">
+        <h1>Tom's Bar</h1>
+            <ul id="example-1">
+                <li v-for="item in items">
+                {{ item.name }}
+                </li>
+            </ul>
+    </div>
+</template>
+
+<script>
+export default {
+name: 'drink-list',
+
+data() {
+    return {
+      items: [
+      { name: 'old fashioned'},
+      { name: 'sazerac' },
+      
+    ]
+    }
+  }
+}
+</script>
+ 
+<style>
+
+</style>
